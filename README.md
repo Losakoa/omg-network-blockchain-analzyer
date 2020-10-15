b220e6519ce8366237c05674e80748c048d5e626
Carlos <c.feliciano79@gmail.com>
2020-10-15 11:14:34 -0700

    fixed up readme a bit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# OMG Network blockchain analyzer w/ functions to get information from the blockchain.
+# OMG Network Blockchain Analyzer 
 
+# Summary
+## This script print out information about OMG Network tokens and their contract on the Etherium Network.
+
+
+## To run the script:
 * Remember to install web3
-	`pip install web3` or `pip3 install web3`
+	`pip install web3` 
+	`pip3 install web3`
+
+* Within json_files dir, change the infura_url with your own infura_url under infura_url.json
--- omg-network-analyzer.py
+++ omg-network-analyzer.py
@@ -1,27 +1,29 @@
 #!/bin/env python
 
+# imports
 from web3 import Web3
 import json
 
-#Vars
-infura_url = "<INSERT INFURA URL HERE"
-web3_url = Web3(Web3.HTTPProvider(infura_url))
+# Vars
 omg_abi_file = "json_files/omg_abi.json"
-infura_url = "json_files/infura_url.json"
-address = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"
+infura_url_file = "json_files/infura_url.json"
+
+# address for the main OMG Network contract
+address = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07" 
 
 
-# def load_infura_url():
-# 	try:
-# 		with open(infura_url_file) as infura:
-# 			infura_url = json.load(infura)
-# 			return infura_url
-# 	except ValueError:
-# 		print("The infura url failed to load correctly.")
+def load_infura_url():
+	try:
+		with open(infura_url_file) as infura:
+			infura_url = json.load(infura)
+			return infura_url["infura_url"]
+	except ValueError:
+		print("The infura url failed to load correctly.")
 
 #is the connection established?
 
 def load_abu_json_file():
+	#abi is a json array which describes the smart contract
 	try:
 		with open(omg_abi_file) as f:
 			abi_data = json.load(f)
@@ -34,50 +36,66 @@ def test_web_connection():
 	try:
 		are_we_connected = web3_url.isConnected()
 		if are_we_connected == True:
-			print("Connection to the etherium network is establishe...\n")
+			print("Connection to the etherium network is established...\n")
 	except ConnectionError:
 		print("Connection to the ehterium network has failed...\n")
 
 
 def build_contract(address,abi_data):
+	""" address is the address of the deployed contract on the blockchain.  With these two pieces of information, we can recontract the smart contract in python and interact with it - https://etherscan.io/token/0xd26114cd6EE289AccF82350c8d8487fedB8A0C07 """
 	try:
 		built_contract = web3_url.eth.contract(address=address,abi=abi_data)
 		return built_contract
 	except ValueError:
 		print("Failed to build contract correctly, please try again.")
-	
 
-def get_latest_eth_block():
-	latest_eth_block = web3_url.eth.getBlock('latest')
-	print(f"Here is the latest OMG block information on the OMG Network:\n\t {latest_eth_block}")
 
 def get_latest_eth_block_number():
 	block_num = web3_url.eth.blockNumber
-	print(f"Here is the latest block number for the OMG Network: \n\t {block_num}")
+	print(f"Here is the latest block number for the OMG Network: \n\t {block_num}\n")
+
+
+def get_latest_eth_block():
+	try:
+		latest_eth_block = web3_url.eth.getBlock('latest')
+		print(f"Here is the latest OMG block information on the OMG Network:\n\t {latest_eth_block}")
+	except ValueError:
+		print("We are unable to pull the latest OMG block information from the network.")
+
+
 
 
 
 if __name__ == "__main__":
-	test_web3 = test_web_connection()
+	""" load the URL, pass it to the web3 library, test the connection, and load the ABU data from the JSON file (found on etherscan.com)"""
+
+	# assigning functions to vars
+	infura_url = load_infura_url()
+	web3_url = Web3(Web3.HTTPProvider(infura_url))
+	test_web3_connection = test_web_connection()
 	abi_data = load_abu_json_file()
-	#abi is a json array which describes the smart contract
-	#address is the address of the deployed contract on the blockchain
-	# with these two pieces of information, we can recontract the smart contract in python and interact with it
+	contract = build_contract(address,abi_data)
+	omg_token = contract.functions.name().call()
+	
+	
 
-	 #https://etherscan.io/token/0xd26114cd6EE289AccF82350c8d8487fedB8A0C07)
-
-	# contract = web3_url.eth.contract(address=address,abi=abi_data) #assigning contract to 
-	contract = build_contract(address,abi_data) 
-	get_latest_eth_block_number()
+	# calling functions
+	#prints token name
+	print(f"TokenName:\n\t {omg_token}\n")
+	# prints total suppy of the token
+	total_supply = contract.functions.totalSupply().call()
+	print(f"Total supply of OMG Network tokens: \n\t {web3_url.fromWei(total_supply, 'ether')}\n")
+	#prints latest block
+	latest_block_num = get_latest_eth_block_number()
+	# dumps out information on the latest OMG Network ehterium block
 	get_latest_eth_block()
 	
 
-	omg_token = contract.functions.name().call()
-	print(omg_token)
-	total_supply = contract.functions.totalSupply().call()
-	print(web3_url.fromWei(total_supply, 'ether'))
+	
+	
 
-	#prints the amount someone owns at the following address
-	customer_supply = contract.functions.balanceOf("0xd26114cd6EE289AccF82350c8d8487fedB8A0C07").call()
-	print(web3_url.fromWei(customer_supply,"ether"))
+
+	# #prints the amount someone owns at the following address
+	# customer_supply = contract.functions.balanceOf("0xd26114cd6EE289AccF82350c8d8487fedB8A0C07").call()
+	# print(web3_url.fromWei(customer_supply,"ether"))
 
